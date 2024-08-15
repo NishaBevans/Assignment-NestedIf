bdf56bcf864e83927c2ebc2a0c86f78b7e30a980
Nisha Bevans <nisha.chavez@yahoo.com>
2024-08-15 11:23:17 -0500

    Updated Commit

--- QuickDecisions-TheEventPlanner.py
+++ QuickDecisions-TheEventPlanner.py
@@ -1,5 +1,14 @@
 #Task 1: Code Correction You are provided with a Python script that is supposed to help in event planning, but it has errors. Identify and fix them.
 
-attendees = input("Enter number of attendees: ")
-venue = "large hall" if attendees >= 100 else "conference room"
-print(venue)
+attendees = int(input("Enter number of attendees: "))
+venue = "large hall" 
+print ("large hall") if attendees > 100 else print("conference room")
+
+#Task 2: Venue Selection: Based on the code from Task 1, further enhance your code to recommend additional things like "audio system" or "projector" based on the number of attendees.
+
+
+
+#Task 3: Catering Choices: Ask the user if they want "vegetarian" food. Recommend "Veggie Delight Caterers" if yes, otherwise recommend "Gourmet Meals Caterers".
+
+
+
